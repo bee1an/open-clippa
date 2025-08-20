cb145bb579f4d3de0e61d05a94a6e17682c1b633
bee1an <beelan@yeah.net>
2025-08-20 10:32:13 +0800

    chore: update train file structure

--- packages/timeline/src/train/train.ts
+++ packages/timeline/src/train/train.ts
@@ -1,61 +1,12 @@
-import type { Rail } from './rail'
+import type { Rail } from '../rail'
+import type { TrainDragStatus, TrainEvents, TrainOption } from './types'
 import { drag, EventBus, getMsByPx, getPxByMs } from '@clippa/utils'
 import { Container, Graphics, Text } from 'pixi.js'
-import { State } from './state'
+import { State } from '../state'
 
 export const TRAIN_HEIGHT = 36
 
 export const RESIZE_TRIGGER_WIDTH = 16
-
-export interface TrainOption {
-  start: number
-  duration: number
-}
-
-export type TrainEvents = {
-  /**
-   * 拖拽开始
-   */
-  moveStart: []
-
-  /**
-   * 每一次移动前
-   *
-   * 这里使用对象的形式是为了让触发事件的时候可以修改这个参数
-   */
-  beforeMove: [{ xValue: number }, target: Train]
-
-  /**
-   * 拖拽结束
-   */
-  moveEnd: [target: Train]
-
-  /**
-   * 左侧调整器的值变化前
-   */
-  beforeLeftResize: [{ xValue: number, wValue: number, disdrawable: boolean }, target: Train]
-
-  /**
-   * 右侧调整器pointerdown
-   */
-  rightResizeStart: [target: Train]
-
-  /**
-   * 右侧调整器的值变化前
-   */
-  beforeRightResize: [{ wValue: number, disdrawable: boolean }, target: Train ]
-
-  /**
-   * 右侧调整器pointerup
-   */
-  rightResizeEnd: [target: Train]
-}
-
-export type TrainDragStatus
-  = | 'normal' // 常规态
-    | 'static' // 静态
-    | 'translucent' // 半透明
-    | 'free' // 游离
 
 let i = 0
 export class Train extends EventBus<TrainEvents> {
@@ -177,11 +128,12 @@ export class Train extends EventBus<TrainEvents> {
       },
       move: (_, { dx, dy }) => {
         const site = { xValue: this.x + dx, yValue: this.y + dy }
+
         this.emit('beforeMove', site, this)
 
         if (this.dragStatus !== 'static') {
           // 静态态不允许移动
-          this.container.x = site.xValue
+          this.container.x = Math.max(site.xValue, 0)
         }
 
         if (this.dragStatus === 'free') {
---
+++ packages/timeline/src/train/types.ts
@@ -0,0 +1,51 @@
+import type { Train } from './train'
+
+export interface TrainOption {
+  start: number
+  duration: number
+}
+
+export type TrainEvents = {
+  /**
+   * 拖拽开始
+   */
+  moveStart: []
+
+  /**
+   * 每一次移动前
+   *
+   * 这里使用对象的形式是为了让触发事件的时候可以修改这个参数
+   */
+  beforeMove: [{ xValue: number }, target: Train]
+
+  /**
+   * 拖拽结束
+   */
+  moveEnd: [target: Train]
+
+  /**
+   * 左侧调整器的值变化前
+   */
+  beforeLeftResize: [{ xValue: number, wValue: number, disdrawable: boolean }, target: Train]
+
+  /**
+   * 右侧调整器pointerdown
+   */
+  rightResizeStart: [target: Train]
+
+  /**
+   * 右侧调整器的值变化前
+   */
+  beforeRightResize: [{ wValue: number, disdrawable: boolean }, target: Train ]
+
+  /**
+   * 右侧调整器pointerup
+   */
+  rightResizeEnd: [target: Train]
+}
+
+export type TrainDragStatus
+  = | 'normal' // 常规态
+    | 'static' // 静态
+    | 'translucent' // 半透明
+    | 'free' // 游离
